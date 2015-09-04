2f61107df667eb772cdee5ac20b0bbaf8c6d9b9a
Aria Stewart <aredridel@dinhe.net>
2015-09-04 19:14:14 -0400

    Track the username signed in with, and add a few more locators

--- signup.js
+++ signup.js
@@ -4,14 +4,18 @@ var urlOf = require('./lib/url');
 tap.test("Sign up a user", function(t) {
   require('./lib/sharedNemo').then(function(nemo) {
     nemo.driver.get(urlOf('/'));
-    nemo.view.signup.signupLink().click();
-    nemo.view.signup.usernameWaitVisible().sendKeys('test' + Date.now());
+    nemo.view.nav.signupLink().click();
+    nemo.view.signup.usernameWaitVisible().sendKeys(nemo.state.desiredUsername || 'test-' + Date.now());
     nemo.view.signup.password().sendKeys('test123');
     nemo.view.signup.verify().sendKeys('test123');
     nemo.view.signup.email().sendKeys('aria+test@npmjs.com');
     nemo.view.signup.makeItSo().click();
     nemo.view.page.h1WaitVisible();
-    return nemo.view.page.h1TextEquals("edit your profile").then(t.pass);
+    nemo.view.page.h1TextEquals("edit your profile").then(t.pass);
+    return nemo.view.nav.username().getText().then(function(text) {
+      t.ok(text);
+      nemo.state.username = text;
+    });
   }).catch(function(error) {
     t.error(error);
     t.bailout();
