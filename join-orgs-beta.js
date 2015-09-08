0157c7ab2dc904167339eeb8311afcb959f9860b
Aria Stewart <aredridel@dinhe.net>
2015-09-08 17:37:25 -0400

    Complete the join-org-beta test

--- join-orgs-beta.js
+++ join-orgs-beta.js
@@ -2,22 +2,24 @@ var tap = require('tap');
 var urlOf = require('./lib/url');
 var P = require('bluebird');
 
-require('./signup');
-require('./logout');
+require('./lib/sharedNemo').then(function(nemo) {
+  nemo.state.desiredUsername = 'test-org-' + Date.now();
 
-tap.test('join beta', function(t) {
-  require('./lib/sharedNemo').then(function(nemo) {
-    return nemo.driver.get(urlOf('/orgs?join-beta')).then(function() {
-      return P.all([
-        nemo.view.login.nameWaitVisible(),
-        nemo.view.login.name().sendKeys(nemo.state.username),
-        nemo.view.login.password().sendKeys('test123'),
-        nemo.view.login.loginButton().click(),
-        nemo.view.nav.usernameWaitVisible()
-      ]).then(t.ok);
-    });
-  }).catch(function(error) {
-    t.error(error);
-    t.bailout();
-  }).then(t.end);
+  require('./signup');
+  require('./logout');
+
+  tap.test('join beta', function(t) {
+    return P.all([
+      nemo.driver.get(urlOf('/orgs?join-beta')),
+      nemo.view.login.nameWaitVisible(),
+      nemo.view.login.name().sendKeys(nemo.state.username),
+      nemo.view.login.password().sendKeys('test123'),
+      nemo.view.login.loginButton().click(),
+      nemo.view.nav.usernameWaitVisible(),
+      nemo.view.org.bannerInfoTextEquals('Organizations are here!')
+    ]).then(t.ok).catch(function(error) {
+      t.error(error);
+      t.bailout();
+    }).then(t.end);
+  });
 });
