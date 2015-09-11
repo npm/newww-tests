43f6cccce414a8806807aa438b0f5611e10adbc2
Aria Stewart <aredridel@dinhe.net>
2015-09-11 00:52:04 -0400

    Factor out common bits for use in other places

--- joinwhoshiring.js
+++ joinwhoshiring.js
@@ -1,7 +1,9 @@
 var Nemo = require('nemo');
 
 var tap = require('tap');
-var urlOf = require('./lib/url.js');
+var urlOf = require('./lib/url');
+var pass = require('./lib/pass');
+var fillStripeForm = require('./lib/fillStripeForm');
 
 var P = require('bluebird');
 
@@ -12,39 +14,11 @@ require('./lib/sharedNemo').then(function(nemo) {
     P.all([
       nemo.driver.get(urlOf('/joinwhoshiring')).then(pass(t, 'loaded page')),
       nemo.view.joinwhoshiring.oneMonthLink().click().then(pass(t, 'clicked form')),
-      fillStripeForm(t).then(function() {
+      fillStripeForm(t, nemo).then(function() {
         return nemo.view.joinwhoshiring.successTextWaitVisible().then(pass(t, 'got success'));
       }).then(pass(t, 'form filled'))
     ]).catch(t.error).then(t.end);
   });
-
-  function fillStripeForm(t) {
-    return nemo.view.stripeCheckout.frame().then(function() {
-      return nemo.view.stripeCheckout.frame();
-    }).then(function(el) {
-      return nemo.driver.switchTo().frame(el).then(pass(t, 'switched frame'));
-    }).then(function() {
-      return P.all([
-        nemo.view.joinwhoshiring.emailWaitVisible().sendKeys('test' + Date.now() + '@npmjs.com').then(t.pass),
-        nemo.view.joinwhoshiring.cardNumberWaitVisible().sendKeys('4242'),
-        nemo.view.joinwhoshiring.cardNumber().sendKeys('4242'),
-        nemo.view.joinwhoshiring.cardNumber().sendKeys('4242'),
-        nemo.view.joinwhoshiring.cardNumber().sendKeys('4242'),
-        nemo.view.joinwhoshiring.ccExp().sendKeys('12'),
-        nemo.view.joinwhoshiring.ccExp().sendKeys('16'),
-        nemo.view.joinwhoshiring.cvc().sendKeys('513'),
-        nemo.view.joinwhoshiring.submit().click().then(pass(t, 'submitted payment'))
-      ]).then(function() {
-        nemo.driver.switchTo().defaultContent()
-      });
-    });
-  }
-
-  function pass(t, message) {
-    return function() {
-      t.pass(message);
-    };
-  }
 
   tap.test("Pay for three months of who's hiring", {
     bail: true
@@ -52,7 +26,7 @@ require('./lib/sharedNemo').then(function(nemo) {
     P.all([
       nemo.driver.get(urlOf('/joinwhoshiring')).then(t.pass),
       nemo.view.joinwhoshiring.threeMonthsLink().click().then(t.pass),
-      fillStripeForm(t).then(function() {
+      fillStripeForm(t, nemo).then(function() {
         return nemo.view.joinwhoshiring.successTextWaitVisible();
       }).then(t.pass)
     ]).catch(t.error).then(t.end);
